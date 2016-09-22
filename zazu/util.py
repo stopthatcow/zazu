ed87e1278f31563d12a9853616be7f368cb4e795
I <nhwiles@gmail.com>
2016-09-22 16:10:41 -0700

    don't override type in prompt()
    (feature/STI-624_edit_prompt)

--- zazu/util.py
+++ zazu/util.py
@@ -7,9 +7,9 @@ __copyright__ = "Copyright 2016, Lily Robotics"
 import gnureadline
 
 
-def prompt(text, default=None, type=str):
+def prompt(text, default=None, expected_type=str):
     if default is not None:
         result = raw_input('{} [{}]: '.format(text, default)) or default
     else:
         result = raw_input('{}: '.format(text))
-    return type(result)
+    return expected_type(result)
