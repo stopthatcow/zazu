a10ad0638bc32fb23cd7f0b2262aa66c390c67af
Nicholas Wiles <nwiles@google.com>
2017-04-04 20:32:53 -0700

    update pytest versions

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setuptools.setup(
         zazu=zazu.cli:cli
         ''',
     setup_requires=[]+pytest_runner,
-    tests_require=['pytest==3.0.5',
+    tests_require=['pytest==3.0.7',
                    'pytest-cov==2.4.0',
                    'pytest-mock==1.5.0'],
 
