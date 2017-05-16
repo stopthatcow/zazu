88ef39d13b59f422fe2ad818c82ae214475e78ba
Nicholas Wiles <nwiles@google.com>
2017-05-15 18:23:40 -0700

    Add test for github_code_review
    (feature/54_add_code_review_plugin)

---
+++ tests/test_github_code_reviewer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import zazu.plugins.github_issue_tracker
+
+__author__ = "Nicholas Wiles"
+__copyright__ = "Copyright 2017"
+
+
+def test_github_issue_tracker():
+    result = zazu.plugins.github_issue_tracker.GithubIssueTracker.from_config({'owner': 'foo',
+                                                                               'repo': 'bar'})
+    assert result._org == 'foo'
+    assert result._repo == 'bar'
