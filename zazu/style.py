8c0220471a84a1a7c323e6d12a8e3ec1cca7344f
Nicholas Wiles <nhwiles@gmail.com>
2017-07-01 21:41:55 -0700

    remove dead code
    (feature/75_stage_styling)

--- zazu/style.py
+++ zazu/style.py
@@ -8,9 +8,7 @@ zazu.util.lazy_import(locals(), [
     'difflib',
     'functools',
     'os',
-    'subprocess',
-    'tempfile'
-
+    'subprocess'
 ])
 
 __author__ = "Nicholas Wiles"
@@ -22,44 +20,14 @@ default_exclude_paths = ['build',
                          'dependencies']
 
 
-class threadsafe_iter:
-    """Takes an iterator/generator and makes it thread-safe by
-    serializing call to the `next` method of given iterator/generator.
-    """
-
-    def __init__(self, it):
-        self.it = it
-        self.lock = threading.Lock()
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        with self.lock:
-            return self.it.next()
-
-
-def threadsafe_generator(f):
-    """A decorator that takes a generator function and makes it thread-safe.
-    """
-    def g(*a, **kw):
-        return threadsafe_iter(f(*a, **kw))
-    return g
-
-
-@threadsafe_generator
-def file_contents_generator(files):
-    for f in files:
-        with open(f, 'r') as f:
-            yield f.read()
-
-
 def read_file(path):
+    """Read a file and return its contents as a string."""
     with open(path, 'r') as f:
         return f.read()
 
 
 def write_file(path, _, styled_string):
+    """Write styled_string string to a file."""
     with open(path, 'w') as f:
         return f.write(styled_string)
 
